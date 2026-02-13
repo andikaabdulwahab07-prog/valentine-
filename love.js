75d1a935549e79f5213405bd6a3b56ef5df1d1ef
andikaabdulwahab07-prog <andikaabdulwahab07@gmail.com>
2026-02-14 04:16:40 +0700

    Update love.js

--- love.js
+++ love.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Typewriter effect
-  const fullText = "my dearest love, you know that i love you more than words can ever express. every moment without you feels incomplete, and my heart aches with the depth of my feelings for you. you're the light in my life, the one who makes everything brighter and more beautiful. i cherish your kindness, your maturity, and the way you always understand me, even when i'm being a bit childish. i'm sorry if i annoy you sometimes i just can't help but show my affection. but most of all, i miss you terribly and dream of the day we can finally be together. please, i want to meet as soon as possible i can't wait to hold you close and create more memories. i love you endlessly. HAPPY VALENTINE'S DAY 💘";
+  const fullText = "my dearest love, you know that i love you more than words can ever express. every moment without you feels incomplete, and my heart aches with the depth of my feelings for you. you're the light in my life, the one who makes everything brighter and more beautiful. i cherish your kindness, your maturity, and the way you always understand me, even when i'm being a bit childish. i'm sorry if i annoy you sometimes i just can't help but show my affection. but most of all, i miss you terribly and dream of the day we can finally be together. please, i want to meet as soon as possible i can't wait to hold you close and create more memories. i love you endlessly. HAPPY VALENTINE'S DAY SAYAANGG 💘";
   let index = 1;
   function typeWriter(){
     if(index<fullText.length){
@@ -34,4 +34,5 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   setTimeout(typeWriter,500);
 });
+
 
